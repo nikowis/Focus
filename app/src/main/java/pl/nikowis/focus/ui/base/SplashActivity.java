4596c6b9241696ba9b0b02daedde96c75eb81160
nikowis <nikowis@gmail.com>
2017-04-29 22:35:24 +0200

    Added checking for internet connection on the splash screen.

--- app/src/main/java/pl/nikowis/focus/ui/base/SplashActivity.java
+++ app/src/main/java/pl/nikowis/focus/ui/base/SplashActivity.java
@@ -1,10 +1,18 @@
 package pl.nikowis.focus.ui.base;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import pl.nikowis.focus.R;
 import pl.nikowis.focus.ui.facebook.FacebookLikesLoader;
 
@@ -12,19 +20,38 @@ public class SplashActivity extends AppCompatActivity {
 
     private static final int SPLASH_DISPLAY_LENGTH = 1000;
     private FacebookLikesLoader likesLoader;
+    @BindView(R.id.retry_connection_button)
+    Button retryConnectionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
 
+        checkIfOnlineAndRedirect();
+    }
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                startMainActivity();
-            }
-        }, SPLASH_DISPLAY_LENGTH);
+    @OnClick(R.id.retry_connection_button)
+    public void checkIfOnlineAndRedirect(){
+        if(isOnline()) {
+            retryConnectionButton.setVisibility(View.INVISIBLE);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startMainActivity();
+                }
+            }, SPLASH_DISPLAY_LENGTH);
+        } else {
+            retryConnectionButton.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
     }
 
     private void startMainActivity() {
